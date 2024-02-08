9e35ddc145bcf9576bc0e3232c1d99b00ccf66ab
soft-felipe <felipem791@gmail.com>
2024-02-07 21:59:49 -0300

    chore: add config basica da api

---
+++ src/test/java/com/felipemoreira/desafioanotaai/DesafioAnotaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipemoreira.desafioanotaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAnotaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
