a36597f91b39aff0eae0566c69394cca29657db2
soft-felipe <felipem791@gmail.com>
2024-02-09 00:49:21 -0300

    feat: add records dto

---
+++ src/main/java/com/felipemoreira/desafioanotaai/domain/category/CategoryDTO.java
@@ -0,0 +1,3 @@
+package com.felipemoreira.desafioanotaai.domain.category;
+
+public record CategoryDTO(String title, String description, String ownerId) {}
---
+++ src/main/java/com/felipemoreira/desafioanotaai/domain/product/ProductDTO.java
@@ -0,0 +1,3 @@
+package com.felipemoreira.desafioanotaai.domain.product;
+
+public record ProductDTO(String title, String description, String ownerId, Integer price, String categoryId) {}
