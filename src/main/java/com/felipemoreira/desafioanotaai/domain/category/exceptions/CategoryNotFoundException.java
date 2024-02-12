bb5333eb300da7ae88d65cb0acf40bad907c21aa
soft-felipe <felipem791@gmail.com>
2024-02-12 02:02:42 -0300

    feat: add messages nas exceptions

--- src/main/java/com/felipemoreira/desafioanotaai/domain/category/exceptions/CategoryNotFoundException.java
+++ src/main/java/com/felipemoreira/desafioanotaai/domain/category/exceptions/CategoryNotFoundException.java
@@ -1,4 +1,10 @@
 package com.felipemoreira.desafioanotaai.domain.category.exceptions;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class CategoryNotFoundException extends RuntimeException {
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
 }
