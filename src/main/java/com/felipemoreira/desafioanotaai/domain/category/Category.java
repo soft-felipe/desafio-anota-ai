5ad2bb045a3b5c2bf1207f9b60a9c65da042e97e
soft-felipe <felipem791@gmail.com>
2024-03-05 15:19:16 -0300

    feat: sobrescrevendo toString

--- src/main/java/com/felipemoreira/desafioanotaai/domain/category/Category.java
+++ src/main/java/com/felipemoreira/desafioanotaai/domain/category/Category.java
@@ -3,6 +3,7 @@ package com.felipemoreira.desafioanotaai.domain.category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -20,5 +21,16 @@ public class Category {
         this.title = data.title();
         this.description = data.description();
         this.ownerId = data.ownerId();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put("id", this.id);
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("type", "category");
+        return json.toString();
     }
 }
--- src/main/java/com/felipemoreira/desafioanotaai/domain/product/Product.java
+++ src/main/java/com/felipemoreira/desafioanotaai/domain/product/Product.java
@@ -1,9 +1,9 @@
 package com.felipemoreira.desafioanotaai.domain.product;
 
-import com.felipemoreira.desafioanotaai.domain.category.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -17,12 +17,26 @@ public class Product {
     private String description;
     private String ownerId;
     private Integer price; /* Salvando sem vírgulas, armezando o número multiplicado por 100 e extraindo dividindo por 100 */
-    private Category category;
+    private String categoryId;
 
     public Product(ProductDTO data) {
         this.title = data.title();
         this.description = data.description();
         this.ownerId = data.ownerId();
         this.price = data.price();
+        this.categoryId = data.categoryId();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put("id", this.id);
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("price", this.price);
+        json.put("categoryId", this.categoryId);
+        json.put("type", "product");
+        return json.toString();
     }
 }
